5d5c1e32a1dd0563a90f3ce061fc921ef631f696
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-03-30 18:13:18 +0100

    api key added to add collection

--- routes/newCollectionRoute.js
+++ routes/newCollectionRoute.js
@@ -13,30 +13,17 @@ router.post('/addcollection', (req, res) => {
     })
   }
 
-  // let securekey = req.query.apiKey;
-  // if (securekey === '123456') {
-
-  if (!req.body.userID) {
-    return errorHandler("userID missing");
-  }
-
-  if (!req.body.collectionName) {
-    return errorHandler("collectionName missing");
-  }
-
-  if (!req.body.albumIDArray) {
-    return errorHandler("albumIDArray missing");
-  }
+  // check body contains all required variables
+  if (!req.body.apiKey) return errorHandler("API key missing");
+  if (!req.body.userID) return errorHandler("userID missing");
+  if (!req.body.collectionName) return errorHandler("collectionName missing");
+  if (!req.body.albumIDArray) return errorHandler("albumIDArray missing");
 
   let user_id = req.body.userID;
   let collection_name = req.body.collectionName;
   let album_ids = req.body.albumIDArray
+  let api_key = req.body.apiKey
   const currentDateTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
-
-  let insertCollection = `
-    INSERT INTO collection (collection_id, collection_name, collection_creation_date, collection_last_edit_date, user_id) 
-    VALUES (NULL, ?, ?, ?, ?);
-    `
 
   connection.getConnection(function (err, conn) {
     if (err) {
@@ -48,48 +35,65 @@ router.post('/addcollection', (req, res) => {
         return errorHandler(err);
       }
 
-      conn.query(insertCollection, [collection_name, currentDateTime, currentDateTime, user_id], (err, result1) => {
+      let checkApiKey = `SELECT EXISTS(SELECT * FROM dev WHERE api_key = ?) AS count`
+      conn.query(checkApiKey, [api_key], (err, result0) => {
         if (err) {
           return conn.rollback(() => {
             errorHandler(err);
           })
         }
-        let insertAlbums = `INSERT INTO collection_album (collection_album_id, album_num, collection_id, album_id) VALUES (NULL, ?, ?, ?)`
-        for (i = 1; i < album_ids.length; i++) {
-          insertAlbums += `, (NULL, ?, ?, ?)`
-        }
-        let collection_id = result1.insertId;
-        let inputs = [];
-        for (i = 0; i < album_ids.length; i++) {
-          inputs.push(i + 1)
-          inputs.push(collection_id)
-          inputs.push(album_ids[i])
+        if (result0[0].count == 0) {
+          return conn.rollback(() => {
+            errorHandler("API key not valid");
+          })
         }
 
-        conn.query(insertAlbums, inputs, (err, result2) => {
+        let insertCollection = `
+      INSERT INTO collection (collection_id, collection_name, collection_creation_date, collection_last_edit_date, user_id) 
+      VALUES (NULL, ?, ?, ?, ?);`
+        conn.query(insertCollection, [collection_name, currentDateTime, currentDateTime, user_id], (err, result1) => {
           if (err) {
             return conn.rollback(() => {
               errorHandler(err);
             })
           }
+          let insertAlbums = `INSERT INTO collection_album (collection_album_id, album_num, collection_id, album_id) VALUES (NULL, ?, ?, ?)`
+          for (i = 1; i < album_ids.length; i++) {
+            insertAlbums += `, (NULL, ?, ?, ?)`
+          }
+          let collection_id = result1.insertId;
+          let inputs = [];
+          for (i = 0; i < album_ids.length; i++) {
+            inputs.push(i + 1)
+            inputs.push(collection_id)
+            inputs.push(album_ids[i])
+          }
 
-          conn.commit((err) => {
+          conn.query(insertAlbums, inputs, (err, result2) => {
             if (err) {
               return conn.rollback(() => {
                 errorHandler(err);
               })
-            } else {
-              res.json({
-                success: true,
-                message: "collection successfully added",
-                itemsAdded: {
-                  user_id,
-                  collection_id,
-                  collection_name,
-                  album_ids,
-                }
-              })
             }
+
+            conn.commit((err) => {
+              if (err) {
+                return conn.rollback(() => {
+                  errorHandler(err);
+                })
+              } else {
+                res.json({
+                  success: true,
+                  message: "collection successfully added",
+                  itemsAdded: {
+                    user_id,
+                    collection_id,
+                    collection_name,
+                    album_ids,
+                  }
+                })
+              }
+            })
           })
         })
       })
