1abe0b8a925021f81a6355d77810ec6ab6242197
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-04-17 10:53:21 +0100

    pagination for all collections

--- routes/collectionsAllRoute.js
+++ routes/collectionsAllRoute.js
@@ -6,11 +6,13 @@ const dataHandler = require('./handlers').dataHandler
 
 router.get('/collections', (req, res) => {
 
-  let page = req.query.page || 0;
+  let frontEndPageNum = parseInt(req.query.page)
+
+  let mySQLPageNum = frontEndPageNum - 1;
 
   let resultsPerPage = 12;
 
-  let pagestart = page * resultsPerPage;
+  let pagestart = mySQLPageNum * resultsPerPage;
   let pageend = pagestart + resultsPerPage;
 
   let sql = `
@@ -22,6 +24,7 @@ router.get('/collections', (req, res) => {
   SELECT collection_id, cover_image_url FROM collection_album
     INNER JOIN album on collection_album.album_id=album.album_id
     ORDER BY album_num;
+  SELECT COUNT(*) AS count FROM collection;
   `
 
   connection.query(sql, [pagestart, pageend], (err, response) => {
@@ -30,21 +33,29 @@ router.get('/collections', (req, res) => {
     } else {
       let collections = response[0]
       let albums = response[1]
+      let count = response[2]
 
       // add albums to collections array
       collections.forEach(collection => {
 
         collection.albumImages = [];
 
-        // add artists to albums array
+        // add images to albums array
+        let loop = 0;
         albums.forEach(album => {
-          if (collection.collection_id == album.collection_id) {
+          if (collection.collection_id == album.collection_id && loop < 4) {
             collection.albumImages.push(album.cover_image_url)
+            loop++;
           }
         })
       })
 
-      res.json(dataHandler("Successfully loaded collections", collections))
+      let dataPacket = dataHandler("Successfully loaded collections", collections)
+      dataPacket.page = frontEndPageNum;
+      dataPacket.totalResults = count[0].count
+      dataPacket.totalPages = Math.ceil(dataPacket.totalResults / resultsPerPage)
+
+      res.json(dataPacket)
     }
 
   })
